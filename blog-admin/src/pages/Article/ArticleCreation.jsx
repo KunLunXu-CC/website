40939029df3f96ae12d753f0229cd202458fc4c6
qianyin925 <884820152@qq.com>
2019-01-29 10:46:22 +0800

    mjs支持

--- blog-admin/src/pages/Article/ArticleCreation.jsx
+++ blog-admin/src/pages/Article/ArticleCreation.jsx
@@ -1,4 +1,33 @@
 import React from 'react';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/specialUrl"
+});
+
+
+
+
+client
+  .query({
+    query: gql`
+      query{
+        getTagList(
+          params: {name: "标签"}
+          page: {page: 1, pageSize: 10}
+        ){
+          list{name, icon id, parent {id name}}
+          page{page pageSize total}
+          change{name}    
+          rescode
+          message
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 class ArticleCreation extends React.Component{
   render(){
