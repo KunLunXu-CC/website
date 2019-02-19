0d4cf264587a533ed440a3d038ec37f9183f24c7
qianyin925 <884820152@qq.com>
2019-02-19 21:19:34 +0800

    [blog-admin]ico修改

--- blog-admin/src/hook/useOptionsHook.js
+++ blog-admin/src/hook/useOptionsHook.js
@@ -5,15 +5,20 @@ const Option = Select.Option;
 
 /**
  * 获取 options
+ * - stats    请求查询返回的统计数据
+ * - page     请求参数（page{page: 获取多少页数据， pageSize: 每次每页请求个数}）
+ * - params   请求参数
+ * - list     请求返回的列表
  */
 export const useOptionsHook = ({ model }) => {
   const [stats, setStats] = useState({ total: 0, totalPage: 1 });
   const [page, setPage] = useState({ page: 1, pageSize: 10 });
   const [params, setParams] = useState({ ids:[], name: '' });
-  const [options, setOptions] = useState([]);
   const [list, setList] = useState([]);
 
-  // 获取 options
+  /**
+   * 内置请求数据函数
+   */
   const getData = useCallback(() => {
     getOptions({ model, page, params }).then(res => {
       setList(res.list);
@@ -21,30 +26,42 @@ export const useOptionsHook = ({ model }) => {
     });
   });
 
-  // 初始化 params
+  /**
+   * 初始化 store： 初始化请求参数（params page） 
+   */
   const init = useCallback(() => {
     setPage({ page: 1, pageSize: 10 });
     setParams({ ids: [], name: ''});
   }, []);
 
-  // 重置 params
-  const resetParams = useCallback((reParams) => {
-    setParams({...params, ...reParams});
+  /**
+   * 重设params
+   * @param {Object} value 需要重新设置的字段
+   */
+  const resetParams = useCallback((value) => {
+    setParams({...params, ...value});
     setPage({ page: 1, pageSize: page.pageSize });
   }, [params, page]);
 
-  // 重置 page (只对 page 进行修改， 不对pageSize 进行修改)
-  const resetPage = useCallback(({ page: rePage }) => {
-    setPage({ page: rePage });
-  }, []);
+  /**
+   * 重设 page: 只对 page 进行修改， 不对pageSize 进行修改
+   * @param {Number} value 要请求多少页的数据
+   */
+  const resetPage = useCallback((value) => {
+    setPage({ ...page, page: value });
+  }, [page]);
 
+
+  // 监听 list 的变化计算 options
+  const options = useMemo(
+    () => list.map(v => (<Option key={v.id} value={v.id}>{v.name}</Option>)), 
+    [list]
+  );
+
+  // 监听 page 的变化
   useEffect(() => {
     getData();
   }, [page]);
-
-  useEffect(() => {
-    setOptions(list.map(v => (<Option key={v.id} value={v.id}>{v.name}</Option>)));
-  }, [list]);
 
   return {
     init,
