2553a7b1fc266137eb9bd0834730d4bfd263a1cb
qianyin925 <884820152@qq.com>
2019-03-24 11:41:31 +0800

    【blog - admin】静态服务的搭建 https://www.tapd.cn/58837035/s/1000026 --story=1000080 --user=潜隐

--- blog-server/app/middleware/index.js
+++ blog-server/app/middleware/index.js
@@ -1,8 +1,14 @@
 /** 放一些比较杂的中间件 */
 const bodyParser = require('koa-bodyparser');
+const staticServe = require('koa-static');
 const logger = require('./logger');
+const path = require('path');
 
 module.exports = (app) => {
+  // 日志
   app.use(logger);
+  // 解析 body
   app.use(bodyParser());
+  // 静态服务（优先于 route 执行， 注意避免路由冲突）
+  app.use(staticServe(path.resolve(__dirname, '../../static')));
 }
