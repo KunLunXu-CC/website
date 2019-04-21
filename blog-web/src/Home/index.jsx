b32c05d17f789a44ea667a60c87dc2ab067e6467
qianyin925 <884820152@qq.com>
2019-04-21 11:35:22 +0800

    desktop

--- blog-web/src/Home/index.jsx
+++ blog-web/src/Home/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import scss from './index.module.scss';
 import Dock from './subpage/Dock';
+import Desktop from './subpage/Desktop';
 
 const Home = (props) => {
   return (
-    <div className={scss['home']}>
+    <Fragment>
       <Dock/>
-      home
-    </div>
+      <Desktop/>
+    </Fragment>
   );
 }
 
---
+++ blog-web/src/Home/subpage/Desktop.jsx
@@ -0,0 +1,62 @@
+import React, {
+  useRef,
+  useMemo,
+  useState,
+  useEffect,
+} from 'react';
+import _ from 'lodash';
+import scss from '../index.module.scss';
+import HomeBg from '@assets/img/home_bg.jpg';
+import BG from '@assets/img/bg.png';
+
+const SCALE = 1920 / 1080;
+
+const useStateHook = () => {
+  const [bgSize, setBgSize] = useState({width: '100%', height: '100%'});
+  const desktopRef = useRef();
+  
+  useEffect(() => {
+    resetBgSize();
+  }, []);
+
+  // 监听事件
+  useEffect(() => {
+    window.addEventListener('resize', resetBgSize);
+    return () => {
+      window.removeEventListener('resize', resetBgSize);
+    }
+  });
+
+  // 重置 bgSize
+  const resetBgSize = () => {
+    const { offsetWidth, offsetHeight } = desktopRef.current;
+    const scale = offsetWidth / offsetHeight;
+    let reset = { width: '100%', height: '100%' };
+    scale < SCALE && (reset = { width: 'auto', height: '100%' });
+    scale > SCALE && (reset = { width: '100%', height: 'auto' });
+    !_.isEqual(bgSize, reset) && setBgSize(reset);
+  }
+
+  // 计算 backgroundSize
+  const backgroundSize = useMemo(() => (
+    `${bgSize.width} ${bgSize.height}`
+  ), [bgSize]);
+
+  return { desktopRef, backgroundSize };
+}
+
+export default () => {
+  const state = useStateHook();
+  return (
+    <div 
+      className={scss['dosktop']}
+      ref={state.desktopRef}
+      style={{
+        backgroundImage: `url(${HomeBg})`,
+        backgroundSize: state.backgroundSize,
+      }}
+    >
+      
+    </div>
+  );
+}
