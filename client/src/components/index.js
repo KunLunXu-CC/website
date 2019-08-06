74727edc004a67693f3208aadc3077a818c1cfc7
qianyin925 <qianyin95@gmail.com>
2019-08-06 10:10:43 +0800

    refactor: docklist 组件抽取 - 样式优化调整

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
+export { default as DockList } from './DockList';
 export { default as Login } from './Login';
 
--- client/src/pages/Home/DockList/index.jsx
+++ client/src/pages/Home/DockList/index.jsx
@@ -5,75 +5,25 @@ import React, {
   useEffect,
   useCallback,
 } from 'react';
-import { Icon } from 'qyrc';
+
 import { useStore } from '@store/index';
-import dockList from '@config/dock';
-import scss from './index.module.scss';
+import { DockList } from '@components';
+import dockConfig from '@config/dock';
 
-const FT = 20;
 const useStateHook = (props, store) => {
-  const [show, setShow] = useState(false);
-  const dockRef = useRef();
-
-  useEffect(() => {
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseleave', onMouseLeave);
-    return () => {
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseleave', onMouseLeave);
-    }
-  });
-
-  // document mousemove 事件
-  const onMouseMove = useCallback((e) => {
-    const { clientHeight } = document.documentElement;
-    const { top } = dockRef.current.getBoundingClientRect();
-    const reset = [
-      e.clientY > top,
-      clientHeight - e.clientY < FT,
-    ].includes(true);
-    reset !== show && setShow(reset);
-  }, [show, dockRef]);
-
-  // document mouseLeave 事件
-  const onMouseLeave = useCallback(() => {
-    setShow(false);
-  }, []);
-
-  // 计算 dock className
-  const dockClassName = useMemo( () => (
-    scss[`dock-${show ? 'show' : 'hidden'}`]
-  ), [show]);
-
-  // 点击事件
   const onClick = (dock) => {
     store.app.open(dock.defaultUrl);
-  }
+  };
+  return { onClick };
+};
 
-  return { dockClassName, onMouseLeave, dockRef, onClick };
-}
-
-const Dock = (props) => {
+export default (props) => {
   const store = useStore();
   const state = useStateHook(props, store);
-
   return (
-    <div ref={state.dockRef} className={state.dockClassName} >
-      <div className={scss['dock-wrapper']}>
-        {
-          dockList.map(v => (
-            <div className={scss['dock-app']} key={v.code}>
-              <div
-                className={scss['dock-app-content']}
-                onClick={state.onClick.bind(null, v)}>
-                <Icon type={v.icon} style={{ fontSize: '50px' }}/>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-    </div>
+    <DockList 
+      dataSource={dockConfig}
+      onClick={state.onClick}
+    />
   );
-}
-
-export default Dock;
+};
