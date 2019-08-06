260b2028aa29ac020c60f2eb6fd15bec90434ccc
qianyin925 <qianyin95@gmail.com>
2019-08-06 17:40:58 +0800

    build: 分离出 service 端

--- server/app/db/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const mongo = require('../../utils/mongo');
-
-module.exports = (app) => {
-  // 为 koa 上下文挂载变量 db
-  app.context.db = {
-    mongo: mongo(path.resolve(__dirname, '../../models'))
-  };
-}
--- server/app/service/common/index.js
+++
@@ -1,3 +0,0 @@
-const { requireFiles } = require('../../../utils');
-const path = require('path');
-module.exports = requireFiles({ dir: path.resolve(__dirname, '.') });
