831d60a3870ee8009341f5b097c0642a93bb3487
qianyin925 <qianyin95@gmail.com>
2019-08-06 10:24:42 +0800

    refactor: dockerlist 组件抽取动画优化

--- client/src/components/DockList/index.jsx
+++ client/src/components/DockList/index.jsx
@@ -11,33 +11,44 @@ import classNames from 'classnames';
 import scss from './index.module.scss';
 
 const FT = 20;
+
 const useStateHook = (props) => {
   const [show, setShow] = useState(false);
   const dockRef = useRef();
 
   useEffect(() => {
+    if (!dockRef.current){return false;}
+    let preShow = show;
+
+    // 1. 重置 show 状态
+    const resetShow = (value) => {
+      if (value === preShow) { return false;}
+      preShow = value;
+      setShow(value);
+    };
+
+    // 2. 鼠标移动事件
+    const onMouseMove = (e) => {
+      const { clientHeight } = document.documentElement;
+      const { top } = dockRef.current.getBoundingClientRect();
+      const reset = [
+        e.clientY > top,
+        clientHeight - e.clientY < FT,
+      ].includes(true);
+      resetShow(reset);
+    };
+
+    // 3. 鼠标移除事件
+    const onMouseLeave = () => {
+      resetShow(false);
+    };
+
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseleave', onMouseLeave);
     return () => {
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseleave', onMouseLeave);
     }
-  });
-
-  // document mousemove 事件
-  const onMouseMove = useCallback((e) => {
-    const { clientHeight } = document.documentElement;
-    const { top } = dockRef.current.getBoundingClientRect();
-    const reset = [
-      e.clientY > top,
-      clientHeight - e.clientY < FT,
-    ].includes(true);
-    reset !== show && setShow(reset);
-  }, [show, dockRef]);
-
-  // document mouseLeave 事件
-  const onMouseLeave = useCallback(() => {
-    setShow(false);
   }, []);
 
   // 计算 dock className
@@ -50,22 +61,25 @@ const useStateHook = (props) => {
     _.isFunction(props.onClick) && props.onClick(dock);
   }
 
-  return { dockClassName, onMouseLeave, dockRef, onClick };
+  return { dockClassName, dockRef, onClick };
 }
 
 const Dock = (props) => {
   const state = useStateHook(props);
 
   return (
-    <div ref={state.dockRef} className={classNames(state.dockClassName, scss['dock'])} >
+    <div 
+      ref={state.dockRef} 
+      className={classNames(state.dockClassName, scss['dock'])} >
       <div className={scss['dock-body']}>
-        {
-          props.dataSource.map(v => (
-            <div key={v.code} className={scss['dock-app']} onClick={state.onClick.bind(null, v)}>
-              <Icon type={v.icon} style={{ fontSize: '50px' }}/>
-            </div>
-          ))
-        }
+        {props.dataSource.map(v => (
+          <div 
+            key={v.code} 
+            className={scss['dock-app']} 
+            onClick={state.onClick.bind(null, v)}>
+            <Icon type={v.icon} style={{ fontSize: '50px' }}/>
+          </div>
+        ))}
       </div>
     </div>
   );
