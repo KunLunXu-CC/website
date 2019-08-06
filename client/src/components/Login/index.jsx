849a51f338ceacd14d4d3e8617e3788d86841285
qianyin925 <qianyin95@gmail.com>
2019-08-06 08:51:59 +0800

    feat: 用户登录页面绘制

---
+++ client/src/components/Login/index.jsx
@@ -0,0 +1,63 @@
+/**
+ * 用户登录组件
+ * @param {Function} props.onLogin  登录触发事件 ({ account, password }) => {} 
+ * @returns {ReactDOM}
+ */
+
+import _ from 'lodash';
+import React from 'react';
+import { Image } from 'qyrc';
+import { Input, Form, Button, Icon } from 'antd';
+
+import scss from './index.module.scss';
+import Head from '@assets/img/login_head.jpg';
+
+const useStateHook = (props) => {
+  // 登录
+  const onLogin = () => {
+    props.form.validateFieldsAndScroll((errors, values) => {
+      if (!!errors){return false;}
+      _.isFunction(props.onLogin) && props.onLogin({ ...values });
+    });
+  };
+
+  return { onLogin };
+};
+
+const Desktop = (props) => {
+  const state = useStateHook(props);
+  return (
+    <div className={scss['login']}>
+      <div className={scss['login-head']} ><Image src={Head}/></div>
+      <div className={scss['login-form']}>
+        <Form>
+          <Form.Item>
+            {props.form.getFieldDecorator('account', {
+               rules: [{ required: true, message: '请输入账号!' }],
+            })(
+              <Input 
+                size="large" 
+                placeholder="请输入账号"
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}/>
+            )}
+          </Form.Item>
+          <Form.Item>
+            {props.form.getFieldDecorator('password', {
+               rules: [{ required: true, message: '请输入密码!' }],
+            })(
+              <Input 
+                size="large" 
+                placeholder="请输入密码"
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}/>
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button size="large" type="primary" block onClick={state.onLogin}>登录</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default Form.create()(Desktop);
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'qyrc';
 
-// import { Login as C_Login } from '@components';
+import { Login as C_Login } from '@components';
 
 import scss from './index.module.scss';
 import HomeBg from '@assets/img/home_bg.jpg';
@@ -9,8 +9,9 @@ import HomeBg from '@assets/img/home_bg.jpg';
 const Login = (props) => {
   return (
     <Image src={HomeBg} className={scss['login']}>
-      {/* <C_Login /> */}
-      1111111111111
+      <C_Login onLogin={({ account, password }) => {
+        console.log('account, password', account, password);
+      }}/>
     </Image>
   );
 }
