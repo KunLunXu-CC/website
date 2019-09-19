6768ae00ff912efe998bad032be1eb72a7131f94
qianyin925 <qianyin95@gmail.com>
2019-09-19 09:59:24 +0800

    refactor: 文章 app 修改

--- src/app/Article/Body/ListPage/index.jsx
+++ src/app/Article/Body/ListPage/index.jsx
@@ -3,10 +3,27 @@ import scss from './index.module.scss';
 
 import Item from './Item';
 
+const mockdata = [
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+  { time: '', title: '', views: '', comments: '', tag: '', desc: '' },
+];
+
 export default () => {
   return (
     <div className={scss['search-page']}>
-      <Item />
+      {
+        mockdata.map((v, index) => (
+          <Item align={index % 2 !== 0 ? 'right' : 'left'}/>
+        ))
+      }
     </div>
   );
 }
