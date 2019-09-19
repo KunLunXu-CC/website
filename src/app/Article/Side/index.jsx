b5f95d282de13720da19e5297661614ffe509f9f
qianyin925 <qianyin95@gmail.com>
2019-09-19 12:38:49 +0800

    refactor: 文章 app 样式布局调整

--- src/app/Article/Side/index.jsx
+++ src/app/Article/Side/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-
+import { Scroll } from 'qyrc';
 import scss from './index.module.scss';
 
 export default () => {
   return (
-    <div>
-      测试
-    </div>
+    <Scroll className={scss['side']}>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+      测试<br/><br/><br/>
+    </Scroll>
   );
 }
