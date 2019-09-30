f2cf59bb98f026998707a20e4c213fc1628883b1
qianyin925 <qianyin95@gmail.com>
2019-09-30 17:39:02 +0800

    refactor: 文件上传设置单独的 timeout 测试是否有效

--- src/api/photo.js
+++ src/api/photo.js
@@ -8,6 +8,7 @@ export const uploadPhotos = ({ body }) => {
     data: formData,
     method: 'post',
     url: '/photo/upload',
+    timeout: 1000 * 60 * 30,
     headers: {"Content-Type": "multipart/form-data"},
   }).then(res => {
     return res.data.data.login;
