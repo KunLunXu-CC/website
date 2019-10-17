cfcd98f4dabaac51178fdf86f13f5457410265cc
qianyin925 <qianyin95@gmail.com>
2019-10-17 13:26:11 +0800

    refactor: 编辑器: 菜单栏

--- src/app/Editor/Menu/index.jsx
+++ src/app/Editor/Menu/index.jsx
@@ -1,5 +1,6 @@
-import React, { 
+import React, {
   useState,
+  Fragment,
   useEffect, 
 } from 'react';
 import { Menu } from 'antd';
@@ -9,37 +10,55 @@ import { useObserver } from 'mobx-react-lite';
 import { useStore } from '../store';
 import scss from './index.module.scss';
 
-const useStateHook = (props, store) => {
-  useEffect(() => {
+const data = [
+  { name: 'JS', id: 'id-js', children: [
+    { name: 'JS1', id: 'id-js1' },
+    { name: 'JS2', id: 'id-js2' },
+    { name: 'JS3', id: 'id-js3' },
+  ]},
+  { name: 'CSS', id: 'id-css', children: [
+    { name: 'CSS1', id: 'id-css1'},
+    { name: 'CSS2', id: 'id-css2'},
+    { name: 'CSS3', id: 'id-css3'},
+    { name: 'CSS4', id: 'id-css4'},
+  ]},
+];
 
+const useStateHook = (props, store) => {
+
+  // 渲染菜单列表
+  const renderMenuList = () => {
+    const recursion = (item) => {
+      const { name, children, id } = item;
+      return children ? 
+        <Menu.SubMenu key={id} title={<div>
+          <Icon type="icon-jiantou" className={scss['menu-arrow']}/>
+          <Icon type="icon-wenjianjia"/>{name}</div>}>
+          {children.map(v => (recursion(v)))}
+        </Menu.SubMenu> :
+        <Menu.Item key={id}>{name}</Menu.Item>;
+    }
+    return data.map(v => (recursion(v)))
+  }
+
+  useEffect(() => {
+    
   }, []);
-  return {};
+
+  return { renderMenuList };
 }
 
 export default (props) => {
   const store = useStore();
   const state = useStateHook(props, store);
+
   return useObserver(() => (
     <div className={scss['menu']}>
       <Scroll className={scss['menu-middle']}>
         <Menu
           mode="inline"
-          defaultSelectedKeys={['all']}
           inlineCollapsed={false}>
-
-          <Menu.SubMenu key="1" title="Submenu">
-            <Menu.Item key="1-1">Option 1-1</Menu.Item>
-            <Menu.Item key="1-2">Option 1-2</Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu key="2" title="Submenu">
-            <Menu.Item key="2-1">Option 2-1</Menu.Item>
-            <Menu.Item key="2-2">Option 2-2</Menu.Item>
-          </Menu.SubMenu>
-
-          <Menu.Item>
-            <Icon type="icon-wenjianjia"  className="anticon"/>
-            <span>JS</span>
-          </Menu.Item>
+          {state.renderMenuList()}
         </Menu>
       </Scroll>
     </div>
