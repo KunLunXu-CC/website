c101ce2653e5e74047dfb2d4d662f17621eb7734
qianyin925 <qianyin95@gmail.com>
2019-11-06 09:10:38 +0800

    refactor: 【client】代码风格 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,5 +48,34 @@ module.exports = {
     "array-bracket-spacing": 1,                                       // 方括号[]里不要加空格
     "key-spacing": 1,                                                 // 在对象的属性中， 键值之间要有空格
     "no-trailing-spaces": 1,                                          // 行末不要空格
+
+    "no-new-object": 1,                                               // 禁止使用 Object 构造函数: new Object()
+    "object-shorthand": 1,                                            // 要求对象字面量简写语法, {fun(){}}, 该规则不标记对象字面量中的箭头函数
+
+    "no-array-constructor": 1,                                        // 禁止使用 Array 构造函数
+    "array-callback-return": 1,                                       // 强制数组方法的回调函数中有 return 语句
+    "prefer-destructuring": [1, {"object": true, "array": true},      // 优先使用数组和对象解构 
+    "quotes": [1, "single"],                                          // 字符串使用要求使用单引号
+    "prefer-template": 1,                                             // 建议使用模板字面量而非字符串连接
+    "template-curly-spacing": [1, "never"],                           // 禁止模板字符串中的嵌入表达式周围空格的使用
+    "no-eval": 1,                                                     // 禁用 eval()
+    "no-useless-escape": 1,                                           // 禁用不必要的转义
+    "func-style": [1, "expression"],                                  // 要求使用函数表达式而不是函数声明
+    "no-loop-func": 1,                                                // 禁止循环中存在函数
+    "prefer-rest-params": 1,                                          // 建议使用剩余参数代替 arguments
+    "no-new-func": 1,                                                 // 禁用Function构造函数 
+
+    "space-before-blocks": [1, "always"],                             // 要求语句块之前的空格 
+    "space-before-function-paren": [1, "always"],                     // 要求函数圆括号之前有一个空格
+    "no-param-reassign": [1, { "props": true }],                      // 禁止对函数参数再赋值、禁止对参数对象修改属性
+    "prefer-spread": 1,                                               // 建议使用扩展语法而非.apply()
+    "prefer-arrow-callback": 1,                                       // 要求使用箭头函数作为回调
+    "arrow-spacing": [1, { "before": true, "after": true }],          // 要求箭头函数的箭头之前或之后有空格
+    "arrow-parens": [1, "as-needed"],                                 // 箭头函数中: 在可以省略括号的地方强制不使用括号
+    "arrow-body-style": [1, "as-needed"],                             // 箭头函数中: 当大括号是可以省略的，强制不使用它们
+    "no-confusing-arrow": 1,                                          // 禁止在可能与比较操作符相混淆的地方使用箭头函数
+    "implicit-arrow-linebreak": [1, "beside"],                        // 禁止在箭头函数体之前出现换行
+
+    // 箭头函数完
   } 
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export { default as NoteLayout } from './NoteLayout';
 export { default as DockList } from './DockList';
 // export { default as TagMenu } from './TagMenu';
 export { default as Login } from './Login';
-
