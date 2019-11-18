7e935da74ffb593a0984733df2a9f8628502f417
qianyin925 <qianyin95@gmail.com>
2019-11-18 13:26:37 +0800

    refactor: 桌面添加加载静态背景以及错误背景

--- src/pages/Home/Desktop/Background/index.jsx
+++ src/pages/Home/Desktop/Background/index.jsx
@@ -3,9 +3,15 @@ import { Image } from 'qyrc';
 
 import scss from './index.module.scss';
 import HomeBg from '@assets/img/home_bg.jpg';
+import Error from './Error';
+import Loading from './Loading';
 
 export default props => (
-  <Image src={HomeBg} className={scss.dosktop}>
+  <Image
+    src={HomeBg}
+    error={<Error/>}
+    loading={<Loading/>}
+    className={scss.background}>
     {props.children}
   </Image>
 );
