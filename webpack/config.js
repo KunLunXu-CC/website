65a16a1960311bbc798a0402492f00753cfdba2f
qianyin925 <qianyin95@gmail.com>
2019-11-19 18:39:23 +0800

    feat: 更新秘钥

--- webpack/config.js
+++ webpack/config.js
@@ -19,13 +19,13 @@ module.exports.globalConsts = {
   production: {
     _DEV_: false,
     PUBLICKEY: JSON.stringify(`-----BEGIN PUBLIC KEY-----
-    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA53tjLpmlsKyzFfM2p6c0
-    p8I1xbab0wF1xnu/l0N+QwZCsZ4r7kC2k6M+ye94zZijPsaFsktWGFFeoUaAe452
-    ByZ1c6XkSdF9rGbsv2lcHi4P3MBfpOC56c6P6bOcuHMupgPtopW6v9Knhvix4lkU
-    xczGtw/ENkKtxYj8LQfHPdlRGaQyAx8HM+5NqPrtafrQC8WMbjfdeW7vmDymQc0m
-    E9eyeRy3nM3yLj2bVkzKIgrL9o5UzNXt1jFB6UWGZ2EX7KW2nZM6HBccm8Nm6WP7
-    36qKTjeivbsbuEE3lcDD19AoMV2aILrzhz/eUfyueIbzjFttO8BAzmcI1q11FYwu
-    OQIDAQAB
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqVIMWB12bjxGa+ZtWcVF
+    AfZ4A5y9uYR+2qJc26t8W57vc9PlvG2HgFlpXnQbA1B8rFhxTlVQh0W9eJY19Pz8
+    eDTfeuaUxL8DR9fECBd8yUUXlm2iGnry86gsm0/Xc6ejni7kigvJok4sCbxMFSIJ
+    AR+PGbhMxC1ZdeVP6uraIw3zTwU7N+MemhxMqBmgmbWU8HGJyCuR2JlY5PxEGNaB
+    JiwOxRukeGY/rQU/Uzb9NczCRhsIJH3ftcnEO73dT9BZqbjeS+YB0UgIuTrtijKX
+    pZMdnbfc1hvi/BfsxR2PYhREn1s0qD1WgA2nvLLURgKCAFtwVViJ0NhNldjS6TxN
+    0QIDAQAB
     -----END PUBLIC KEY-----`),
     GLOBAL_SERVICE: {
       HOST: JSON.stringify('http://www.qianyin925.com:4000'),
