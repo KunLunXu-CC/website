545c3678f32e7fdfd29706c940973aa037c928e8
qianyin925 <qianyin95@gmail.com>
2019-12-31 09:08:26 +0800

    feat: 【client】【文章阅读】没有更多数据提示

--- src/app/Article/Body/ListPage/index.jsx
+++ src/app/Article/Body/ListPage/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Scroll } from 'qyrc';
-import { useObserver } from 'mobx-react-lite';
-
 import Item from './Item';
-import { useStore } from '../../store';
+import NoMore from './NoMore';
 import scss from './index.module.scss';
+
+import { Empty } from 'antd';
+import { Scroll } from 'qyrc';
+import { useStore } from '../../store';
+import { useObserver } from 'mobx-react-lite';
 
 export default () => {
   const store = useStore();
   return useObserver(() => (
     <Scroll className={scss.scroll}>
-      {store.article.list.map((value, index) => (
-        <Item
-          data={value}
-          key={value.id}
-          align={index % 2 !== 0 ? 'right' : 'left'}
-        />
-      ))}
-      <br/>
+      {store.article.list.length > 0 ?
+        store.article.list.map((value, index) => (
+          <Item
+            data={value}
+            key={value.id}
+            align={index % 2 !== 0 ? 'right' : 'left'}
+          />
+        )) :
+        <Empty/>
+      }
+      <NoMore/>
     </Scroll>
   ));
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import reactDom from 'react-dom';
-
-import Store from '@store/index';
+import moment from 'moment';
 import Router from './Route';
+import Store from '@store/index';
+import reactDom from 'react-dom';
+import zhCN from 'antd/es/locale/zh_CN';
+
+import { ConfigProvider } from 'antd';
 
 import '@assets/font/fonticon/iconfont';
+import 'moment/locale/zh-cn';
 import './console';
 
-reactDom.render((
-  <Store>
-    <Router />
-  </Store>
-), document.getElementById('root'));
+moment.locale('zh-cn');
+
+const App = (
+  <ConfigProvider locale={zhCN}>
+    <Store>
+      <Router />
+    </Store>
+  </ConfigProvider>
+);
+
+reactDom.render(App, document.getElementById('root'));
