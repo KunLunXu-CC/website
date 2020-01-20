4a88b8ab63b3920ff543d13e00651d3d0ed67626
qianyin925 <qianyin95@gmail.com>
2020-01-20 15:57:39 +0800

    build: 【client】移除 mobx

--- src/model/application/effects.js
+++
@@ -1,8 +0,0 @@
-// import * as services from './services';
-// import { take, put,call, takeEvery } from 'redux-saga/effects';
-// import { computed } from 'mobx';
-// import dockConfig from '@config/dock';
-
-// export function * takeEveries() {
-//   // yield takeEvery('app/openApp', openApp);
-// };
