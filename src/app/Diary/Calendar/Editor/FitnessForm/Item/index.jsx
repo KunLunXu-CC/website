2e91f44438af8f5ec26e15745d92ee2f24682446
qianyin925 <qianyin95@gmail.com>
2020-05-18 10:30:22 +0800

    refactor: 【client】数字字典应用绘制

--- src/app/Diary/Calendar/Editor/FitnessForm/Item/index.jsx
+++ src/app/Diary/Calendar/Editor/FitnessForm/Item/index.jsx
@@ -4,7 +4,6 @@ import React, {
   useEffect,
 } from 'react';
 import scss from './index.module.scss';
-import fitnessProjects from '@config/fitnessProjects';
 
 import { useSelector } from 'react-redux';
 import { DIARY_EDITOR_DIARY } from '../../../../consts';
@@ -14,13 +13,11 @@ import {
   Row,
   Form,
   Select,
-  InputNumber,
 } from 'antd';
-import { FITNESS_TYPE, FITNESS_PLACE, FITNESS_FEEL } from '@config/consts';
+import { FITNESS_TYPE, FITNESS_PLACE } from '@config/consts';
 import { Icon } from 'qyrc';
 
 const useStateHook = props => {
-  const [projects, setProjects] = useState([]);
   const [showPlaceField, setShowPlaceField] = useState(false);
 
   // 弹窗
@@ -44,38 +41,6 @@ const useStateHook = props => {
     ))
   ), []);
 
-  // 训练感受下拉项
-  const feelOptions = useMemo(() => (
-    Object.values(FITNESS_FEEL).map(V => (
-      <Select.Option value={V.VALUE} key={V.VALUE}>
-        {V.DESC}
-      </Select.Option>
-    ))
-  ), []);
-
-  // 训练项目下拉项
-  const projectOptions = useMemo(() => (
-    projects.map(v => (
-      <Select.Option value={v.value} key={v.value}>
-        {v.desc}
-      </Select.Option>)
-    )
-  ), [projects]);
-
-  // 重置 projects
-  const resetProjects = (type, place) => {
-    const currentType = _.isNumber(type)
-      ? type
-      : props.form.getFieldValue(['fitness', props.field.fieldKey, 'type']);
-    const currentPlace = _.isNumber(place)
-      ? place
-      : props.form.getFieldValue(['fitness', props.field.fieldKey, 'place']);
-    const newProjects = fitnessProjects.filter(
-      v => v.type === currentType && v.place === currentPlace
-    );
-    setProjects(newProjects);
-  };
-
   // 是否显示 place 字段
   const resetShowPlaceField = type => {
     const currentType = _.isNumber(type)
@@ -84,21 +49,12 @@ const useStateHook = props => {
     setShowPlaceField(currentType === FITNESS_TYPE.ANAEROBIC.VALUE);
   };
 
-  // place 修改
-  const onPlaceChange = place => {
-    const fitness = props.form.getFieldValue(['fitness']);
-    fitness[props.field.fieldKey].project = void 0;
-    props.form.setFieldsValue(fitness);
-    resetProjects(void 0, place);
-  };
-
   // type 修改
   const onTypeChange = type => {
     const fitness = props.form.getFieldValue(['fitness']);
     fitness[props.field.fieldKey].project = void 0;
     fitness[props.field.fieldKey].place = void 0;
     props.form.setFieldsValue({ fitness });
-    resetProjects(type);
     resetShowPlaceField(type);
   };
 
@@ -108,23 +64,15 @@ const useStateHook = props => {
         modal,
         `diary.fitness[${props.field.fieldKey}].type`
       );
-      const place = _.get(
-        modal,
-        `diary.fitness[${props.field.fieldKey}].place`
-      );
-      resetProjects(type, place);
       resetShowPlaceField(type);
     }
   }, [modal]);
 
   return {
     typeOptions,
-    feelOptions,
     placeOptions,
     onTypeChange,
-    onPlaceChange,
     showPlaceField,
-    projectOptions,
   };
 };
 
@@ -146,8 +94,7 @@ export default props => {
               }]}
               name={[field.name, 'type']}
               className={scss['form-item']}
-              fieldKey={[field.fieldKey, 'type']}
-            >
+              fieldKey={[field.fieldKey, 'type']}>
               <Select
                 placeholder="类型"
                 style={{ width: '100%' }}
@@ -170,54 +117,12 @@ export default props => {
                 fieldKey={[field.fieldKey, 'place']}>
                 <Select
                   placeholder="训练部位"
-                  style={{ width: '100%' }}
-                  onChange={state.onPlaceChange}>
+                  style={{ width: '100%' }}>
                   {state.placeOptions}
                 </Select>
               </Form.Item>
             </Col> : null
           }
-          <Col span={8}>
-            <Form.Item
-              {... field}
-              label="项目"
-              className={scss['form-item']}
-              name={[field.name, 'project']}
-              fieldKey={[field.fieldKey, 'project']}>
-              <Select
-                mode="multiple"
-                placeholder="锻炼项目"
-                style={{ width: '100%' }}>
-                {state.projectOptions}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item
-              {... field}
-              label="时长 (分钟) "
-              className={scss['form-item']}
-              name={[field.name, 'duration']}
-              fieldKey={[field.fieldKey, 'duration']}>
-              <InputNumber
-                min={0}
-                placeholder="时长"
-                style={{ width: '100%' }}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item
-              {... field}
-              label="感受"
-              name={[field.name, 'feel']}
-              className={scss['form-item']}
-              fieldKey={[field.fieldKey, 'feel']}>
-              <Select style={{ width: '100%' }} placeholder="感受">
-                {state.feelOptions}
-              </Select>
-            </Form.Item>
-          </Col>
         </Row>
       </Col>
       <Col
