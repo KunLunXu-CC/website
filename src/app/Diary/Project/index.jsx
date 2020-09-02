abacb0029a393dc000e1baa61f1e02e67dc5ae8f
qianyin925 <qianyin925@qq.com>
2020-09-02 13:41:25 +0800

    feat(【client】【日记】计划 / 项目 前端页面布局): 【client】【日记】计划 / 项目 前端页面布局

--- src/app/Diary/Project/index.jsx
+++ src/app/Diary/Project/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import scss from './index.module.scss';
+import mock from './mock';
+import Column from './Column';
 
-export default () => (
-  <div className={scss.project}>
-    计划/项目
-  </div>
-);
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
+const useStateHook = () => {
+  const [data, setData] = React.useState(mock);
+
+  const onDragEnd = () => {};
+  return { onDragEnd, data };
+};
+
+export default () => {
+  const state = useStateHook();
+
+  return (
+    <div className={scss.project}>
+      <DragDropContext onDragEnd={state.onDragEnd}>
+        <Droppable
+          droppableId="board"
+          direction="horizontal">
+          {provided => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {state.data.map(v => <Column data={v} key={v.id}/>)}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
