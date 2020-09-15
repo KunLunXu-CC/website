a665caf0389b2434bf1ddc915b80d4f86f1bce84
qianyin925 <qianyin925@qq.com>
2020-09-15 18:08:40 +0800

    feat: 【client】dock 样式重写

--- src/pages/Home/DockList/List/index.jsx
+++ src/pages/Home/DockList/List/index.jsx
@@ -1,18 +1,38 @@
-import React, {
-  useCallback,
-} from 'react';
+import React from 'react';
 import apps from '@app';
 import scss from './index.module.scss';
-
+import styled from 'styled-components';
 import { Icon } from 'qyrc';
 
+const DockApp = styled.div`
+  --margin: ${({ index, currentIndex }) => {
+    const defaultValue = '10px';
+    if (!_.isNumber(currentIndex)) {
+      return defaultValue;
+    }
+    const mapIndex = 2 - Math.abs(currentIndex - index);
+    return ['20px', '30px', '40px'] ?. [mapIndex] || defaultValue;
+  }};
+
+  --scale: ${({ index, currentIndex }) => {
+    const defaultValue = 1;
+    if (!_.isNumber(currentIndex)) {
+      return defaultValue;
+    }
+    const mapIndex = 2 - Math.abs(currentIndex - index);
+    return [1.3, 1.6, 1.9] ?. [mapIndex] || defaultValue;
+  }};
+`;
+
 const useStateHook = props => {
+  const [currentIndex, setCurrentIndex] = React.useState(null);
+
   // 点击事件
-  const onClick = useCallback(dock => {
+  const onClick = React.useCallback(dock => {
     _.isFunction(props.onClick) && props.onClick(dock);
   }, [props]);
 
-  return { onClick };
+  return { onClick, currentIndex, setCurrentIndex };
 };
 
 export default props => {
@@ -20,12 +40,17 @@ export default props => {
 
   return (
     <div className={`${scss.dock} ${scss['dock-auto-hiding']}`}>
-      <div className={scss['dock-body']}>
+      <div
+        className={scss['dock-body']}>
         {props.dataSource.map((v, index) => (
-          <div
+          <DockApp
+            index={index}
             key={v.code || index}
             className={scss['dock-app']}
-            onClick={state.onClick.bind(null, v)}>
+            currentIndex={state.currentIndex}
+            onClick={state.onClick.bind(null, v)}
+            onMouseLeave={state.setCurrentIndex.bind(null, null)}
+            onMouseEnter={state.setCurrentIndex.bind(null, index)}>
             <div className={scss['dock-tooltip']}>
               {apps[v.code].name}
             </div>
@@ -33,7 +58,7 @@ export default props => {
               type={apps[v.code].icon}
               className={scss['dock-icon']}
             />
-          </div>
+          </DockApp>
         ))}
       </div>
     </div>
