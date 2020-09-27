984ee1c9c63f8ecbc3b7012717e34e02bef29be4
qianyin925 <qianyin925@qq.com>
2020-09-27 10:44:54 +0800

    refactor: 【client】【编辑器】代码整理

--- src/app/Editor/consts.js
+++ src/app/Editor/consts.js
@@ -1,4 +1,4 @@
-import { ARTICLE_STATUS, APP_CODE } from '@config/consts';
+import { APP_CODE } from '@config/consts';
 
 // 弹窗类型常量 - 发布
 export const RELEASE_CONFIRM = 'EDITOR_RELEASE_CONFIRM';
@@ -12,19 +12,9 @@ export const THUMB_SETTING = 'EDITOR_THUMB_SETTING';
 // Activity 配置
 export const ACTIVITY_LIST = {
   ALL: {
-    KEY: 'all',
+    KEY: 'blog',
     TITLE: '全部',
     ICON: 'icon-all',
-  },
-  SAVE: {
-    TITLE: '未发布',
-    ICON: 'icon-xiajia',
-    KEY: ARTICLE_STATUS.SAVE,
-  },
-  RELEASE: {
-    TITLE: '已发布',
-    ICON: 'icon-fabu',
-    KEY: ARTICLE_STATUS.RELEASE,
   },
 };
 
