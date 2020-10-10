1acb5264e7775d8230f51f37fbdd7b1e420ab202
qianyin925 <qianyin925@qq.com>
2020-10-10 13:41:59 +0800

    refactor: 【client】【编辑器】代码整理

--- src/app/Editor/SideBar/index.jsx
+++ src/app/Editor/SideBar/index.jsx
@@ -1,42 +1,44 @@
 import React from 'react';
 import Resource from './Resource';
+import classNames from 'classnames';
 import scss from './index.module.scss';
-
 import { VariableContainer } from 'qyrc';
 import { useDispatch, useSelector } from 'react-redux';
 
-const MENU_MIN_WIDTH = 4;  // 菜单最小宽度
+const SIDE_MIN_WIDTH = 4;  // 菜单最小宽度
 
 const useStateHook = () => {
   const dispatch = useDispatch();
-  const { side } = useSelector(state => ({
-    side: state.editor.side,
+  const { collapsed } = useSelector(state => ({
+    collapsed: state.editor.side.collapsed,
   }));
+
+  // 顶层 className
+  const className = React.useMemo(() => classNames(
+    scss.side,
+    { [scss.collapsed]: collapsed }
+  ), [collapsed]);
 
   // 尺寸变化
   const onResize = React.useCallback(({ width }) => dispatch({
     type: 'editor/setSide',
-    side: { collapsed: MENU_MIN_WIDTH === width },
+    side: { collapsed: SIDE_MIN_WIDTH === width },
   }), []);
 
-  return { side, onResize };
+  return { className, onResize };
 };
 
 export default () => {
   const state = useStateHook();
   return (
     <VariableContainer
-      className={scss.side}
       margin={{ right: '20%' }}
       operationList={['right']}
       onResize={state.onResize}
       style={{ height: '100%' }}
-      constraintSize={{ width: MENU_MIN_WIDTH }}>
-      {!state.side.collapsed ?
-        <div className={scss.body}>
-          <Resource/>
-        </div> : null
-      }
+      className={state.className}
+      constraintSize={{ width: SIDE_MIN_WIDTH }}>
+      <Resource/>
     </VariableContainer >
   );
 };
