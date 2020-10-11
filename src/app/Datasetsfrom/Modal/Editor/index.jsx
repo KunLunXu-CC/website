3cbc1e216bb37aeac37870a37bf6f72c4d503162
qianyin925 <qianyin925@qq.com>
2020-10-11 17:30:55 +0800

    feat: 【client】【数据字典】数据父级可配置

--- src/app/Datasetsfrom/Modal/Editor/index.jsx
+++ src/app/Datasetsfrom/Modal/Editor/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import scss from './index.module.scss';
 
+import {
+  Row,
+  Col,
+  Form,
+  Modal,
+  Input,
+  Select,
+  Cascader,
+  InputNumber,
+} from 'antd';
 import { DATASETSFROM_CODE } from '@config/consts';
 import { useSelector, useDispatch } from 'react-redux';
 import { MODAL_CODE_DATASETSFROM_EDITOR } from '../../consts';
-import { Form, Modal, Input, InputNumber, Select } from 'antd';
 
 // 类型下拉选项
 const CODE_OPTIONS = Object.values(DATASETSFROM_CODE).map(v => (
@@ -15,21 +24,32 @@ const CODE_OPTIONS = Object.values(DATASETSFROM_CODE).map(v => (
 
 const useStateHook = () => {
   const dispatch = useDispatch();
-
   const [form] = Form.useForm();
-
-  const { modal, menuSelectedKey } = useSelector(state => ({
+  const { modal, menuSelectedKey, datasetsfrom } = useSelector(state => ({
+    datasetsfrom: state.datasetsfrom,
     modal: state.modal[MODAL_CODE_DATASETSFROM_EDITOR],
     menuSelectedKey: state.datasetsfromManage.menu.selectedKey,
   }));
 
+  // antd Cascader 配置
+  const options = React.useMemo(() => Object.values(DATASETSFROM_CODE)
+    .filter(v => datasetsfrom[v.VALUE])
+    .map(
+      v => ({
+        value: v.VALUE,
+        label: v.DESC,
+        children: datasetsfrom[v.VALUE].map(ele => ({
+          value: ele.id,
+          label: ele.name,
+        })),
+      })
+    ), [datasetsfrom]);
+
   // 取消: 关闭弹窗
-  const onCancel = () => {
-    dispatch({
-      type: 'modal/closeModal',
-      code: MODAL_CODE_DATASETSFROM_EDITOR,
-    });
-  };
+  const onCancel = () => dispatch({
+    type: 'modal/closeModal',
+    code: MODAL_CODE_DATASETSFROM_EDITOR,
+  });
 
   // 确认
   const onOk = async () => {
@@ -52,6 +72,7 @@ const useStateHook = () => {
         value: modal.data ?. value ?? void 0,
         desc: modal.data ?. desc ?? void 0,
         icon: modal.data ?. icon ?? void 0,
+        parent: modal.data ?. parent ?? void 0,
         code: modal.data
           ?. code
           ?? (_.isNumber(menuSelectedKey) ? menuSelectedKey : void 0),
@@ -59,16 +80,16 @@ const useStateHook = () => {
       : form.resetFields();
   }, [modal]);
 
-  return { form, modal, onOk, onCancel };
+  return { form, modal, onOk, onCancel, options };
 };
 
 export default () => {
   const state = useStateHook();
 
   return (
-    <Form form={state.form} labelCol={{ span: 3 }}>
+    <Form form={state.form}>
       <Modal
-        width={600}
+        width={800}
         okText="确定"
         destroyOnClose
         closable={false}
@@ -79,24 +100,40 @@ export default () => {
         visible={!!state.modal}
         onCancel={state.onCancel}
         title={state.modal ?. title}>
-        <Form.Item
-          label="名称"
-          name="name"
-          rules={[{ required: true, message: '请填写字典名称!' }]}>
-          <Input placeholder="字典名称"/>
-        </Form.Item>
-        <Form.Item
-          label="数值"
-          name="value"
-          rules={[{ required: true, message: '请填写字典值!' }]}>
-          <InputNumber placeholder="字典值" style={{ width: '100%' }}/>
-        </Form.Item>
-        <Form.Item
-          name="code"
-          label="类型"
-          rules={[{ required: true, message: '请选择字典类型!' }]}>
-          <Select placeholder="字典类型">{CODE_OPTIONS}</Select>
-        </Form.Item>
+        <Row gutter={40}>
+          <Col span={12}>
+            <Form.Item
+              label="名称"
+              name="name"
+              rules={[{ required: true, message: '请填写字典名称!' }]}>
+              <Input placeholder="字典名称"/>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="数值"
+              name="value"
+              rules={[{ required: true, message: '请填写字典值!' }]}>
+              <InputNumber placeholder="字典值" style={{ width: '100%' }}/>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="code"
+              label="类型"
+              rules={[{ required: true, message: '请选择字典类型!' }]}>
+              <Select placeholder="字典类型">{CODE_OPTIONS}</Select>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="parent"
+              label="父级"
+              rules={[{ required: true, message: '请选择字典类型!' }]}>
+              <Cascader placeholder="字典类型" options={state.options}/>
+            </Form.Item>
+          </Col>
+        </Row>
         <Form.Item label="图标" name="icon">
           <Input placeholder="图标"/>
         </Form.Item>
