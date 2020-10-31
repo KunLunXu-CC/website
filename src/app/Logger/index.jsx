e30f26266daa05bb092a374724359da8684098fd
qianyin925 <qianyin925@qq.com>
2020-10-31 11:20:59 +0800

    refactor: 【client】日志实时查看 - 前后端打通

--- src/app/Logger/index.jsx
+++ src/app/Logger/index.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import scss from './index.module.scss';
+import ws from '@utils/ws';
+
+const wss = ws('/ws/logger');
 
 const useStateHook = () => {
+  const [logger, setLogger] = React.useState([]);
 
   React.useEffect(() => {
-
+    const tmp = [];
+    wss.onmessage = event => {
+      tmp.push(JSON.parse(event.data));
+      setLogger([... tmp]);
+    };
   }, []);
+
+  return { logger };
 };
 
 export default () => {
   const state = useStateHook();
-
   return (
     <div className={scss.logger}>
-
+      <div className={scss.main}>
+        {state.logger.map(((v, index) => (
+          <pre key={index}> {v.message}</pre>
+        )))}
+      </div>
     </div>
   );
 };
