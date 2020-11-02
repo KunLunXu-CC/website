d807d2543dc93f6f82f993dd2a619b7863d58f75
qianyin925 <qianyin925@qq.com>
2020-11-02 09:27:12 +0800

    feat: 【client】日志实时查看 - 前后端打通

--- src/app/Logger/index.jsx
+++ src/app/Logger/index.jsx
@@ -6,23 +6,40 @@ const wss = ws('/ws/logger');
 
 const useStateHook = () => {
   const [logger, setLogger] = React.useState([]);
+  const mainRef = React.useRef();
+
+  // 滚动带底部
+  const onScrollEnd = React.useCallback(() => {
+    if (!mainRef.current) {
+      return false;
+    }
+    const { scrollHeight, clientHeight } = mainRef.current;
+    mainRef.current.scrollTop = scrollHeight - clientHeight;
+  }, [mainRef.current]);
 
   React.useEffect(() => {
     const tmp = [];
+    onScrollEnd();
     wss.onmessage = event => {
+      const { scrollTop, scrollHeight, clientHeight } = mainRef.current;
       tmp.push(JSON.parse(event.data));
       setLogger([... tmp]);
+      // 只有在滚动条位于底部时, 才在每次加载数据时自动滚动
+      scrollTop + clientHeight === scrollHeight &&
+      setTimeout(onScrollEnd, 1000);
     };
   }, []);
 
-  return { logger };
+  return { logger, mainRef };
 };
 
 export default () => {
   const state = useStateHook();
   return (
     <div className={scss.logger}>
-      <div className={scss.main}>
+      <div
+        ref={state.mainRef}
+        className={scss.main}>
         {state.logger.map(((v, index) => (
           <pre key={index}> {v.message}</pre>
         )))}
