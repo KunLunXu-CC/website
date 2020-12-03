79ac61dcf9b3e94e0311092016bc9d9933e96902
qianyin925 <qianyin925@qq.com>
2020-12-03 17:36:30 +0800

    refactor: 【client】系统偏好设置

--- src/app/Setting/Body/index.jsx
+++ src/app/Setting/Body/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import scss from './index.module.scss';
 
-export default () => (
-  <div className={scss.body}>
+import { Checkbox } from 'antd';
+import { useSelector } from 'react-redux';
 
-  </div>
-);
+const useStateHook = () => {
+  const setting = useSelector(state => state.setting);
+
+  // 修改值
+  const onChange = React.useCallback((key, event) => {
+    console.log(key, event);
+  }, []);
+
+  return { setting, onChange };
+};
+
+export default () => {
+  const state = useStateHook();
+
+  return (
+    <div className={scss.body}>
+      <Checkbox
+        checked={state.setting.hideDock}
+        onChange={this.onChange.bind(null, 'hideDock')}>
+        自动隐藏和显示程序坞
+      </Checkbox>
+    </div>
+  );
+};
