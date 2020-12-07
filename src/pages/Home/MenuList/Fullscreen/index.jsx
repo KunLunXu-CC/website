59af988cbc60c38fb866c6b58a0eabeed68d5784
qianyin925 <qianyin925@qq.com>
2020-12-07 17:05:10 +0800

    refactor: 【client】新增配置项目、并调整配置应用界面

--- src/pages/Home/MenuList/Fullscreen/index.jsx
+++ src/pages/Home/MenuList/Fullscreen/index.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
+import useFullscreen from '@utils/useFullscreen';
 
 import { Icon } from 'qyrc';
-import {
-  isFullscreen,
-  exitFullscreen,
-  requestFullscreen,
-} from '@utils/fullscreen';
 import { useSelector } from 'react-redux';
 
 const useStateHook = () => {
-  const [fulled, setFulled] = React.useState(false);
+  const { onToggle, fulled } = useFullscreen(document.body);
   const setting = useSelector(state => state.setting.desktop);
-
-  // 切换状态
-  const onToggle = React.useCallback(() => {
-    setFulled(!fulled);
-  }, [fulled]);
-
-  // 监听状态: 根据状态自动切换全屏、退出全屏
-  React.useEffect(() => {
-    isFullscreen() && exitFullscreen();
-    fulled && requestFullscreen(document.getElementById('root'));
-  }, [fulled]);
-
-  // 监听 ctrl + shift + f 切换全屏
-  React.useEffect(() => {
-    const listener = event => {
-      const { shiftKey, ctrlKey, metaKey, keyCode } = event;
-      if (shiftKey && (ctrlKey || metaKey) && keyCode === 70) {
-        event.preventDefault();
-        event.stopPropagation();
-        setFulled(!fulled);
-      }
-    };
-
-    document.addEventListener('keydown', listener);
-    return () => document.removeEventListener('keydown', listener);
-  }, [fulled]);
 
   return { fulled, onToggle, setting };
 };
