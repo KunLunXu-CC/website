604d75c614cbcf9c90d85dd09b9f404ac7c5e207
qianyin925 <qianyin925@qq.com>
2020-12-13 10:34:01 +0800

    refactor: 【client】新增配置项目、并调整配置应用界面

--- src/pages/Home/MenuList/Notification/Target/index.jsx
+++ src/pages/Home/MenuList/Notification/Target/index.jsx
@@ -2,36 +2,48 @@ import React from 'react';
 import moment from 'moment';
 import scss from './index.module.scss';
 
+import { useSelector } from 'react-redux';
+
 // TODO：优化，是否可以放于浏览器进程中
 const useStateHook = () => {
-  const [data, setData] = React.useState(null);
+  const [date, setDate] = React.useState(moment());
+  const setting = useSelector(state => state.setting.menuBar);
 
-  // 获取日期
-  const resetData = React.useCallback(() => {
-    const time = moment().format('HH:mm:ss');
-    const week = ['一', '二', '三', '四', '五', '六', '天'][moment().weekday()];
-    setData({ week, time });
-  }, []);
+  // 星期
+  const week = React.useMemo(() => (
+    setting.showWeek
+      ? `周${['一', '二', '三', '四', '五', '六', '天'][date.weekday()]}`
+      : ''
+  ), [date.format('YYYY-MM-DD HH:mm:ss'), setting.showWeek]);
+
+  // 当前时间
+  const time = React.useMemo(
+    () => date.format(setting.formatDate),
+    [date.format('YYYY-MM-DD HH:mm:ss'), setting.formatDate]
+  );
 
   React.useEffect(() => {
-    resetData();
-    const cleard = setInterval(resetData, 1000);
-    return clearInterval.bind(null, cleard);
-  }, [resetData]);
+    let unmount = false;
+    const resetDate = () => {
+      if (unmount) {
+        return false;
+      }
+      setDate(moment());
+      requestAnimationFrame(resetDate);
+    };
+    resetDate();
+    return () => (unmount = true);
+  }, []);
 
-  return { data };
+  return { week, time };
 };
 
 export default props => {
   const state = useStateHook(props);
   return (
     <div className={scss.wrapper} {... props}>
-      <div className={scss.week}>
-        周{_.get(state, 'data.week')}
-      </div>
-      <div className={scss.time}>
-        {_.get(state, 'data.time')}
-      </div>
+      <div className={scss.week}>{state.week}</div>
+      <div className={scss.time}>{state.time}</div>
     </div>
   );
 };
