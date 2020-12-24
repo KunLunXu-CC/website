5654a152bba8c15fb428580c3cde82efe5d56e4c
qianyin925 <qianyin925@qq.com>
2020-12-24 15:48:38 +0800

    refactor: 【client】【日记】根据类型对账单进行统计

--- src/app/Diary/Stats/Bill/Echart/index.jsx
+++ src/app/Diary/Stats/Bill/Echart/index.jsx
@@ -22,6 +22,17 @@ const useStateHook = props => {
     tooltip: {
       trigger: 'axis',
     },
+    legend: {
+      top: 12,
+      right: 0,
+      data: ['收入', '支出', '结余'],
+    },
+    grid: {
+      top: 50,
+      left: 60,
+      right: 25,
+      bottom: 25,
+    },
     xAxis: {
       type: 'category',
       data: data.xAxis,
@@ -35,17 +46,20 @@ const useStateHook = props => {
         type: 'bar',
         name: '支出',
         data: data.expend,
+        color: '#fc6a11',
       },
       {
         barGap: 0,
         type: 'bar',
         name: '收入',
+        color: '#197afe',
         data: data.income,
       },
       {
         barGap: 0,
         type: 'bar',
         name: '结余',
+        color: '#289322',
         data: data.balance,
       },
     ],
@@ -58,7 +72,6 @@ const useStateHook = props => {
       console.log(... args);
     },
   }]), []);
-
   return { option, on };
 };
 
