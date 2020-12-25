fe50e5a83f8f2624cb16b31b0f3ea5ca827d4a6c
qianyin925 <qianyin925@qq.com>
2020-12-25 10:37:47 +0800

    refactor: 【client】【日记】根据类型对账单进行统计

--- src/app/Diary/Modal/BillDetail/index.jsx
+++ src/app/Diary/Modal/BillDetail/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Echart } from 'qyrc';
 import { Modal, Button } from 'antd';
 import { STATS_BILL_DETAIL } from '../../consts';
 import { useDispatch, useSelector } from 'react-redux';
@@ -13,10 +14,67 @@ const useStateHook = () => {
   const data = React.useMemo(() => {
     const bill = modal?.diaries.reduce((total, ele) => ([
       ... total,
-      ... ele.bill,
+      ... ele.bill.filter(v => v.expend),
     ]), []);
-    console.log('bill ---------->>>>', bill);
+    const gloup = _.groupBy(bill, 'tag.name');
+    const res = [];
+    for (const name in gloup) {
+      res.push({
+        name,
+        value: gloup[name].reduce(
+          (total, ele) => Math.round(total + ele.expend), 0
+        ),
+      });
+    }
+    return res;
   }, [modal]);
+
+  // echart 配置
+  const option = React.useMemo(() => ({
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)',
+    },
+    legend: {
+      right: 10,
+      orient: 'vertical',
+      data: data.map(v => v.name),
+    },
+    series: [
+      {
+        data,
+        type: 'pie',
+        name: '账单详情',
+        radius: ['60%', '90%'],
+        center: ['40%', '50%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center',
+        },
+        emphasis: {
+          label: {
+            show: true,
+            formatter: '{nameStyle|{b}}\n{valueStyle|{c}}',
+            rich: {
+              nameStyle: {
+                fontSize: 14,
+                color: 'rgba(0, 0, 0, 0.6)',
+              },
+              valueStyle: {
+                fontSize: 20,
+                padding: [8, 0, 0, 0],
+                color: 'rgba(0, 0, 0, 0.8)',
+              },
+            },
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+      },
+    ],
+  }), [data]);
 
   // 取消
   const onCancel = () => dispatch({
@@ -24,7 +82,7 @@ const useStateHook = () => {
     type: 'modal/closeModal',
   });
 
-  return { modal, onCancel, data };
+  return { modal, onCancel, data, option };
 };
 
 export default () => {
@@ -38,7 +96,7 @@ export default () => {
       getContainer={false}
       visible={!!state.modal}
       footer={<Button onClick={state.onCancel} type="primary">关闭</Button>}>
-      111111
+      <Echart height={300} option={state.option}/>
     </Modal>
   );
 };
