da5fef0b832d92f08eaeddd7345125ea5a3e1a5d
qianyin925 <qianyin925@qq.com>
2020-12-29 17:15:55 +0800

    refactor: 【qyrc】阅读界面设计

--- src/app/Reader/Detail/Content/index.jsx
+++ src/app/Reader/Detail/Content/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 import { Markdown } from 'qyrc';
+import { SERVICE_STATIC_IMAGE_URL } from '@config/consts';
+
+const options = {
+  overrides: {
+    img: {
+      component: props => (<img src={ /http/.test(props.src)
+        ? props.src
+        : `${SERVICE_STATIC_IMAGE_URL}${props.src}`
+      }/>),
+    },
+  },
+};
 
 export default props => (
-  <Markdown>
+  <Markdown options={options}>
     {props.data || ''}
   </Markdown>
 );
