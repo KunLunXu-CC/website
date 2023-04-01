24c8de8ee6e3abaf9333ef22e7f060a4126c394e
墨渊君 <moyuanjun925@gmail.com>
2023-04-01 10:55:11 +0800

    chore: 修改域名

--- src/console/print/default.js
+++ src/console/print/default.js
@@ -2,11 +2,11 @@
  * 控制台默认打印内容
  */
 $console.log(
-  '%cGitHub%c https://github.com/qianyin925',
-  ` 
+  '%cGitHub%c https://github.com/KunLunXu0-0',
+  `
     color: #fff;
-    padding: 2px 5px; 
+    padding: 2px 5px;
     border-radius: 4px;
-    background: #1890ff; 
+    background: #1890ff;
   `, '',
 );
--- src/pages/Home/MenuList/Github/index.jsx
+++ src/pages/Home/MenuList/Github/index.jsx
@@ -5,7 +5,7 @@ export default () => (
   <a
     target="_blank"
     rel="noopener noreferrer"
-    href="https://github.com/qianyin925">
+    href="https://github.com/KunLunXu0-0">
     <Icon type="icon-github" />
   </a>
 );
