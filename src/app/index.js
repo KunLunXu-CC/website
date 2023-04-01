7529907de055ab94b4ab6c55da6eab170f6ff8a8
墨渊君 <moyuanjun925@gmail.com>
2023-04-01 16:46:25 +0800

    feat: 🎸 应用图标管理

--- src/app/index.js
+++ src/app/index.js
@@ -12,7 +12,7 @@ export default {
   [APP_CODE.AI]: {
     name: 'AI',
     component: AI,
-    icon: 'icon-AI',
+    icon: 'klx.pro.71e88db3660e596de6ded68375b8bb38.svg',
     code: APP_CODE.AI,
     modalProps: {
       dragHeight: 30,
@@ -25,7 +25,7 @@ export default {
     name: '相册',
     component: Album,
     code: APP_CODE.ALBUM,
-    icon: 'icon-banjixiangce',
+    icon: 'klx.pro.6f66375e9c7d528f20c5fa57b3ad1ea0.svg',
     modalProps: {
       dragHeight: 40,
       toolPosition: { top: 8, left: 10 },
@@ -35,7 +35,7 @@ export default {
   [APP_CODE.EDITOR]: {
     name: '编辑器',
     component: Editor,
-    icon: 'icon-daima',
+    icon: 'klx.pro.a0745138b76ad9e1d99e2489297e8131.svg',
     code: APP_CODE.EDITOR,
     modalProps: {
       dragHeight: 30,
@@ -47,7 +47,7 @@ export default {
   [APP_CODE.READER]: {
     name: '阅读',
     component: Reader,
-    icon: 'icon-wenzhang',
+    icon: 'klx.pro.4bc44b31f4d00e5192b7deabf6682cf1.svg',
     code: APP_CODE.READER,
     modalProps: {
       dragHeight: 40,
@@ -58,7 +58,7 @@ export default {
   [APP_CODE.DIARY]: {
     name: '日记',
     component: Diary,
-    icon: 'icon-biji',
+    icon: 'klx.pro.fe12a2f0188beb3fce602354664551ab.svg',
     code: APP_CODE.DIARY,
     modalProps: {
       dragHeight: 20,
@@ -68,8 +68,8 @@ export default {
   },
   [APP_CODE.LOGGER]: {
     name: '日志',
-    icon: 'icon-rizhichaxun',
     component: Logger,
+    icon: 'klx.pro.c2a2a3a8b3e4ddfea309d3e292531d82.svg',
     code: APP_CODE.LOGGER,
     modalProps: {
       dragHeight: 40,
@@ -79,7 +79,7 @@ export default {
   },
   [APP_CODE.SETTING]: {
     name: '系统偏好设置',
-    icon: 'icon-renwucopy',
+    icon: 'klx.pro.38008433a4ff3ef788a5baae4f077483.svg',
     component: Setting,
     code: APP_CODE.SETTING,
     modalProps: {
@@ -93,7 +93,7 @@ export default {
     name: '用户管理',
     component: User,
     code: APP_CODE.USER,
-    icon: 'icon-renwucopy',
+    icon: 'klx.pro.d1fb923369fb7b28217133738e37ba2f.svg',
     modalProps: {
       dragHeight: 40,
       toolPosition: { top: 14, left: 14 },
